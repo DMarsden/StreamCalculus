ed05e4c5686e594a7df5afa479b72da52191da61
Dan Marsden <danmarsden@yahoo.co.uk>
2014-03-29 17:26:08 +0000

    moving to applicative and removing some support functions

--- StreamCalculus.hs
+++ StreamCalculus.hs
@@ -4,27 +4,23 @@ import Control.Applicative
 
 data Stream a = Stream { front :: a,  derivative :: (Stream a) }
 
-zeroes = constant 0
-
-combine binop xs ys = Stream ((front xs) `binop` (front ys)) (combine binop (derivative xs) (derivative ys))
-
 instance Functor Stream where
  fmap f xs = Stream (f $ front xs) (fmap f (derivative xs))
 
 instance Applicative Stream where
  pure x = Stream x (pure x)
- fs <*> xs = (front fs (front xs)) ((derivative fs) (derivative xs))
+ fs <*> xs = Stream (front fs (front xs)) ((derivative fs) <*> (derivative xs))
 
 instance (Num a) => Num (Stream a) where
- xs + ys = combine (+) xs ys
- xs - ys = combine (-) xs ys
- xs * ys = combine (*) xs ys
+ xs + ys =  liftA2 (+) xs ys
+ xs - ys = liftA2 (-) xs ys
+ xs * ys = liftA2 (*) xs ys
  abs xs =  abs <$> xs
  signum xs = signum <$> xs
  fromInteger z = pure $ fromInteger z
 
 instance (Fractional a) => Fractional (Stream a) where
- xs / ys = combine (/) xs ys
+ xs / ys = liftA2 (/) xs ys
  recip xs = recip <$> xs
  fromRational r = pure $ fromRational r
 
@@ -33,8 +29,8 @@ instance (Floating a) => Floating (Stream a) where
  exp xs = exp <$> xs
  sqrt xs = sqrt <$> xs
  log xs = log <$> xs
- xs ** ys = combine (**) xs ys
- logBase xs ys = combine logBase xs ys
+ xs ** ys = liftA2 (**) xs ys
+ logBase xs ys = liftA2 logBase xs ys
  sin xs = sin <$> xs
  cos xs = cos <$> xs
  tan xs = tan <$> xs
