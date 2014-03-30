fa812c73b44dff3fe42f72108544982034809547
Dan Marsden <danmarsden@yahoo.co.uk>
2014-03-30 11:07:14 +0100

    some of the basics of the Rutten scheme, handling the non pointwise operations with separate syntax for now. Could merge the maths with the Hinze scheme except for the fromInteger difference

--- Data/Stream/Core.hs
+++ Data/Stream/Core.hs
@@ -2,15 +2,18 @@ module Data.Stream.Core where
 
 data Stream a = Stream { front :: a, derivative :: (Stream a) }
 
+streamAt :: Integer -> (Stream a) -> a
 streamAt 0 xs = front xs
 streamAt n xs = streamAt (n - 1) (derivative xs)
 
+merge :: (Stream a) -> (Stream a) -> (Stream a)
 merge xs ys = Stream (front xs) (merge ys xs)
 
-
+takeN :: Integer -> Stream a -> [a]
 takeN 0 xs = []
 takeN n xs = (front xs) : (takeN (n-1) (derivative xs))
 
+dropN :: Integer -> (Stream a) -> (Stream a)
 dropN 0 xs = xs
 dropN n xs = dropN (n - 1) (derivative xs)
 
--- Data/Stream/Rutten.hs
+++ Data/Stream/Rutten.hs
@@ -2,13 +2,54 @@ module Data.Stream.Rutten where
 
 import Data.Stream.Core
 
+embed :: (Num a) => a -> (Stream a)
 embed z = Stream z (embed 0)
+
 x = Stream 0 (embed 1)
 
+combine f xs ys = Stream (f (front xs) (front ys)) (combine f (derivative xs) (derivative ys))
+
+(|*|) :: (Num a) => Stream a -> Stream a -> Stream a
+xs |*| ys = Stream (front xs * (front ys)) (((derivative xs) |*| ys) + ((embed $ front xs) |*| (derivative ys)))
+
+(|**|) :: (Num a) => Stream a -> Integer -> Stream a
+xs |**| 0 = fromInteger 1
+xs |**| n = xs |*| (xs |**| (n - 1))
+
+-- We do pointwise operations for these, will handle
+-- special cases such as stream multiplication and exp
+-- using separate syntax to avoid confusion.
+
 instance (Num a) => Num (Stream a) where
- xs + ys = Stream (front xs + (front ys)) (derivative xs + (derivative ys))
- xs - ys = Stream (front xs - (front ys)) (derivative xs - (derivative ys))
+ xs + ys = combine (+) xs ys
+ xs - ys = combine (-) xs ys
+ xs * ys = combine (*) xs ys
  abs xs = fmap abs xs
  signum xs = fmap signum xs
- fromInteger z = embed (fromInteger z)
- xs * ys = Stream (front xs * (front ys)) (((derivative xs) * ys) + ((embed $ front xs) * (derivative ys)))
+-- This is the key difference to the Hinze scheme
+ fromInteger z = embed $ fromInteger z
+
+instance (Fractional a) => Fractional (Stream a) where
+ xs / ys = combine (/) xs ys
+ recip xs = fmap recip xs
+ fromRational r = embed $ fromRational r
+
+instance (Floating a) => Floating (Stream a) where
+ pi = embed pi
+ exp xs = fmap exp xs
+ sqrt xs = fmap sqrt xs
+ log xs = fmap log xs
+ xs ** ys = combine (**) xs ys
+ logBase xs ys = combine logBase xs ys
+ sin xs = fmap sin xs
+ cos xs = fmap cos xs
+ tan xs = fmap tan xs
+ asin xs = fmap asin xs
+ acos xs = fmap acos xs
+ atan xs = fmap atan xs
+ sinh xs = fmap sinh xs
+ cosh xs = fmap cosh xs
+ tanh xs = fmap tanh xs
+ asinh xs = fmap asinh xs
+ acosh xs = fmap acosh xs
+ atanh xs = fmap atanh xs
