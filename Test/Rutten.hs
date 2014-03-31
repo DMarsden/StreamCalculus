3392c89c63dfbab483084ac7f9c1ce05039f3eed
Dan Marsden <danmarsden@yahoo.co.uk>
2014-03-31 13:07:25 +0100

    Rutten tests now using test framework

--- Test/Rutten.hs
+++ Test/Rutten.hs
@@ -1,16 +1,28 @@
 module Test.Rutten where
 
 import Test.QuickCheck
+import Test.Framework
+import Test.Framework.Providers.QuickCheck2
 
 import Data.Stream.Core
 import Data.Stream.Rutten
 
-prop_plusCommutative xs ys = all (test (xs + ys) (ys + xs)) [0..100]
+propPlusCommutative :: Stream Int -> Stream Int -> Bool
+propPlusCommutative xs ys = all (test (xs + ys) (ys + xs)) [0..100]
  where
   test vs ws n = (streamAt n vs) == (streamAt n ws)
 
-prop_fromIntegerFirstElem z = z == streamAt 0 (fromInteger z)
+propFromIntegerFirstElem :: Integer -> Bool
+propFromIntegerFirstElem z = z == streamAt 0 (fromInteger z)
 
-prop_fromIntegerLaterElems z = all (test $ fromInteger z) [1..100]
+propFromIntegerLaterElems :: Integer -> Bool
+propFromIntegerLaterElems z = all (test $ fromInteger z) [1..100]
  where
   test xs n = 0 == streamAt n xs
+
+main = defaultMain tests
+
+tests = [testProperty "plus commutative" propPlusCommutative,
+         testProperty "first element of fromInteger equals the integer" propFromIntegerFirstElem,
+         testProperty "later elements of fromInteger are zero" propFromIntegerLaterElems
+         ]
