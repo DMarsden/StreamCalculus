4d9d0be05806890f0180412bff86c03078a80c69
Dan Marsden <danmarsden@yahoo.co.uk>
2014-03-31 13:03:57 +0100

    adding test framework to the Hinze tests so far

--- Test/Hinze.hs
+++ Test/Hinze.hs
@@ -1,15 +1,25 @@
 module Test.Hinze where
 
 import Test.QuickCheck
+import Test.Framework
+import Test.Framework.Providers.QuickCheck2
 
 import Data.Stream.Core
 import Data.Stream.Hinze
 import Control.Applicative
 
-prop_plusCommutative xs ys = all (test (xs + ys) (ys + xs)) [0..100]
+propPlusCommutative :: Stream Int -> Stream Int -> Bool
+propPlusCommutative xs ys = all (test (xs + ys) (ys + xs)) [0..100]
  where
   test vs ws n = (streamAt n vs) == (streamAt n ws)
 
-prop_pureConstant x = all (test $ pure x) [1..100]
+propPureConstant :: Int -> Bool
+propPureConstant x = all (test $ pure x) [1..100]
  where
   test xs n = x == streamAt n xs
+
+main = defaultMain tests
+
+tests = [testProperty "plus commutative" propPlusCommutative,
+         testProperty "pure constant" propPureConstant
+         ]
